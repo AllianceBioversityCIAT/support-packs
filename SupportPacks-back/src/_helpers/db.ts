7ce7770feba0649c5eba937fcd211a829495fef4
felipelvira <felipe.elvira.garcia@outlook.com>
2020-10-16 15:00:54 -0500

    DB env variables updated

--- SupportPacks-back/src/_helpers/db.ts
+++ SupportPacks-back/src/_helpers/db.ts
@@ -2,6 +2,7 @@ import { Sequelize, DataTypes } from "sequelize";
 import * as dotenv from "dotenv";
 
 dotenv.config();
+console.log(`mysql://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.HOST_DB}:${process.env.DB_PORT}/${process.env.DB}`);
 export const database = new Sequelize(`mysql://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.HOST_DB}:${process.env.DB_PORT}/${process.env.DB}`, {
     dialect: 'mysql',
     define: {
