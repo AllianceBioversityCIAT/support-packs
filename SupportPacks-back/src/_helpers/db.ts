a0d3288b396bda1be42bcd370c68c8af8766c966
felipelvira <felipe.elvira.garcia@outlook.com>
2020-10-20 14:11:19 -0500

    mel-cop project updated

--- SupportPacks-back/src/_helpers/db.ts
+++ SupportPacks-back/src/_helpers/db.ts
@@ -1,7 +1,7 @@
 import { Sequelize, DataTypes } from "sequelize";
 import * as dotenv from "dotenv";
 
-dotenv.config();
+require('dotenv').config();
 export const database = new Sequelize(`mysql://${process.env.USER_DB}:${process.env.PASS_DB}@${process.env.HOST_DB}:${process.env.DB_PORT}/${process.env.DB}`, {
     dialect: 'mysql',
     define: {
