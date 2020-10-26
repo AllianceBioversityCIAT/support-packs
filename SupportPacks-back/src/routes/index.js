dd949d79529e6cfb8f3ccdab4d72552a3dba000f
felipelvira <felipe.elvira.garcia@outlook.com>
2020-10-26 11:13:40 -0500

    JS files removed

--- SupportPacks-back/src/routes/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var express_1 = require("express");
-var AuthRoute_1 = require("./AuthRoute");
-var SPRoute_1 = require("./SPRoute");
-var routes = express_1.Router();
-routes.use("/auth", AuthRoute_1["default"]);
-routes.use("/sp", SPRoute_1["default"]);
-exports["default"] = routes;
