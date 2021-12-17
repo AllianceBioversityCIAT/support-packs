16a4cc151d0fcf7901dd4f64ae7a0794483bef47
Santiago Galvez <santiagogalvez17@gmail.com>
2021-12-17 04:49:22 -0500

    Add connection between filterPanel and results component

--- supportpacks-front/projects/aiccrasp/src/app/home/results/results.component.ts
+++ supportpacks-front/projects/aiccrasp/src/app/home/results/results.component.ts
@@ -1,19 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, Input, OnInit } from '@angular/core';
 import { faBookmark, faClock, faUserCircle } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
-  styleUrls: ['./results.component.scss']
+  styleUrls: ['./results.component.scss'],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(':enter', [
+
+          // css styles at start of transition
+          style({ opacity: 0 }),
+
+          // animation and styles at end of transition
+          animate('.3s', style({ opacity: 1 }))
+        ]),
+        transition(':leave', [
+          // css styles at start of transition
+          style({ opacity: 1 }),
+
+          // animation and styles at end of transition
+          animate('.3s', style({ opacity: 0 }))
+        ])
+      ]
+    )
+  ]
 })
 export class ResultsComponent implements OnInit {
 
   faUserCircle = faUserCircle;
   faClock = faClock;
   faBookmark = faBookmark;
+
+  @Input() filters;
+
+  tools = [
+    {
+      name: "CSA Prioritisation Framework (CSA-PF)",
+      link: "https://ccafs.cgiar.org/resources/tools/climate-smart-agriculture-prioritization-framework",
+      importance_level: "Very important",
+      description: "The CSA Prioritisation Framework (CSA-PF), designed for channelling CSA investments, has the objective to help decision makers identify best-bet CSA investment portfolios that achieve gains in food security, farmers’ resilience to climate change, and low-emissions development in the agriculture sector. The framework is divided into four phases: (i) initial assessment of CSA options; (ii) identification of top CSA options (workshop); (iii) calculation of cost and benefits of top CSA options; and (iv) portfolio development and evaluation of barriers (workshop).",
+      strengths: "Incorporates expert and stakeholder views, often reflective of realities in the field  , Alignment with national programs and policies, Speed to completion, Farmer-centric.",
+      limitations: "Subject to bias if groups are dominated by certain individuals (e.g. women left out)",
+      resources: [
+        {name: "1.1  Climate-smart solutions for Mali", link: "https://hdl.handle.net/10568/72419",type: "Reports and other publications" },
+        {name: "1.2 Assessing climate change adaptation needs in the agricultural sector", link: "https://hdl.handle.net/10568/80014", type: "Reports and other publications"},
+        {name: "1.3 Climate-smart agriculture investment prioritisation framework", link: "https://ccafs.cgiar.org/sites/default/files/projects/attachments/CSA%20Investment%20Prioritization%20Framework%20EN%20Dic2014.pdf",type: "Reports and other publications"},
+        {name: "1.4 CSA prioritisation framework", link: "https://p4s.ccafs.cgiar.org/tools/csa-prioritization-framework",type: "Outreach products"},
+
+      ]
+    }
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  validateFilterData() {
+    if(this.filters) {
+      return this.filters.user !== null && this.filters.phase !== null && this.filters.area !== null;
+    }
+    return false;
   }
 
 }
--- supportpacks-front/projects/aiccrasp/src/app/home/side-menu/side-menu.component.ts
+++ supportpacks-front/projects/aiccrasp/src/app/home/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { faUserCircle, faClock, faBookmark } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -10,26 +10,54 @@ import { faUserCircle, faClock, faBookmark } from '@fortawesome/free-solid-svg-i
 export class SideMenuComponent implements OnInit {
 
   roles = [
-    {name: 'Program Manager'},
-    {name: 'Project Manager'},
-    {name: 'M&E Officer'},
+    { name: 'Program Manager' },
+    { name: 'Project Manager' },
+    { name: 'M&E Officer' },
   ]
+
+  filterData = {
+    user: null,
+    phase: null,
+    area: null,
+  }
+
+  users: any[] = ["Researcher", "Technical staff (public/private)", "Academia"]
+  phases: any[] = ["Design", "Implementation", "Monitoring and Evaluation"]
+  areas: any[] = [
+    "Calculators / Mitigation options",
+    " Climate information services",
+    " Climate risk assessment",
+    " Climate - smart business models",
+    " Gender",
+    " Monitoring reporting and verification",
+    " Program support",
+    " Prioritisation",
+    " Scaling",
+    " Science - policy platform",
+    " Situation analysis",
+    " Targeting / Need assessment"
+  ];
+
   selectedRole;
 
   faUserCircle = faUserCircle;
   faClock = faClock;
   faBookmark = faBookmark;
+
+  @Output() filtersEmitter:EventEmitter<any> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
   selectFilter(type: string, data: any) {
-    // this.filterData[type] = data.name;
+    this.filterData[type] = data;
     // this.filterDataId[type] = data.id;
     // this.filterDataId = Object.assign({}, this.filterDataId);
-    // this.filterData = Object.assign({}, this.filterData);
-    // console.log(this.filterData)
+    this.filterData = Object.assign({}, this.filterData);
+    this.filtersEmitter.emit(this.filterData);
+    console.log(this.filterData)
   }
 
 
