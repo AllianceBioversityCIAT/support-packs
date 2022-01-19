864258da1bdd20584d12145efd1694c4d7684bbb
Santiago Galvez <santiagogalvez17@gmail.com>
2022-01-19 14:48:25 -0500

    Add tools component

--- supportpacks-front/projects/aiccrasp/src/app/tools/tools.module.ts
+++ supportpacks-front/projects/aiccrasp/src/app/tools/tools.module.ts
@@ -3,13 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { ToolsRoutingModule } from './tools-routing.module';
 import { ToolsComponent } from './tools.component';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faChevronLeft, faCoffee, faToggleOff } from '@fortawesome/free-solid-svg-icons';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SortPipe } from '../pipes/sort.pipe';
 
 
 @NgModule({
-  declarations: [ToolsComponent],
+  declarations: [ToolsComponent, SortPipe],
   imports: [
     CommonModule,
-    ToolsRoutingModule
-  ]
+    ToolsRoutingModule,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NgxSpinnerModule,
+    NgbModule
+  ],
+  providers: [SortPipe]
 })
-export class ToolsModule { }
+export class ToolsModule { 
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faChevronLeft, faToggleOff, faToggleOff, faCoffee);
+  }
+}
