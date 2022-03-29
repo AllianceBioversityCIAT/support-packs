f0a54793a2a2f9089c266bb8f79477e84ccb187a
Santiago Galvez <santiagogalvez17@gmail.com>
2022-03-29 09:56:56 -0500

    Update TermsConditions text

--- supportpacks-front/projects/aiccrasp/src/app/home/results/results.component.ts
+++ supportpacks-front/projects/aiccrasp/src/app/home/results/results.component.ts
@@ -47,7 +47,7 @@ import { AiccraToolsService } from '../../services/aiccra-tools.service';
   ]
 })
 export class ResultsComponent implements OnInit {
-  tcAICCRA = 'To continue downloading your files, please first fill in your email and then some basic information.This information will be used by CCAFS solely for impact assessment and CGIAR and Center level reporting purposes.Filling it in will greatly help us to track the use of the portal and keep improving it. This portal provides data to a very large community of users and improving its usability and efficiency is a key aspect we work on continuously. However, you may click on <a class="skip 2" (click)="onSetEmail()">Skip</a> to download links directly.';
+  tcAICCRA = 'To continue downloading your files, please first fill in your email and then some basic information.This information will be used by AICCRA solely for impact assessment and CGIAR and Center level reporting purposes.Filling it in will greatly help us to track the use of the portal and keep improving it. This portal provides data to a very large community of users and improving its usability and efficiency is a key aspect we work on continuously. However, you may click on <a class="skip 2" (click)="onSetEmail()">Skip</a> to download links directly.';
 
   faUserCircle = faUserCircle;
   faClock = faClock;
@@ -167,7 +167,7 @@ export class ResultsComponent implements OnInit {
     this.showSelectedTools = false;
     this.hideTC();
   }
-  
+
   hideTC(ev?) {
     this.tcIsVisible = false;
   }
