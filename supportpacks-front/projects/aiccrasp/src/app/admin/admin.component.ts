bf091024c21d81b9dbf2d95aee96b44f7f352917
Santiago Gálvez <santiagogalvez17@gmail.com>
2022-05-16 06:57:48 -0500

    Restart object form

--- supportpacks-front/projects/aiccrasp/src/app/admin/admin.component.ts
+++ supportpacks-front/projects/aiccrasp/src/app/admin/admin.component.ts
@@ -31,61 +31,61 @@ export class AdminComponent implements OnInit {
   thematicAreas = [];
   resourceTypes = resourceTypes;
   importanceLevels = importanceLevels;
-  requestTool: RequestTool = {
-    fullname: "Santiago Gálvez Lasso",
-    email: "santiagogalvez17@gmail.com",
-    toolName: "My new",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    link: "www.google.com",
-    estimated_time: "3 Months",
-    integrates_gender: null,
-    is_tested_online: null,
-    target_scale: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    participants: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
-    methods: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    input_types: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    limitations: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
-    strengths: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
-    expected_outputs: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
-    human_resources: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
-    key_references: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
-    thematicArea: 2,
-    importanceResearcher: { design: null, implementation: null, me: null },
-    importanceTechnical: { design: null, implementation: null, me: null },
-    importanceAcademia: { design: null, implementation: null, me: null },
-    resources: [
-      {
-        resourceName: "Lorem ipsum",
-        resourceLink: "Lorem ipsum",
-        resourceCategory: "Training materials"
-      }
-    ]
-  };
-
   // requestTool: RequestTool = {
-  //   fullname: null,
-  //   email: null,
-  //   toolName: null,
-  //   description: null,
-  //   link: null,
-  //   estimated_time: null,
+  //   fullname: "Santiago Gálvez Lasso",
+  //   email: "santiagogalvez17@gmail.com",
+  //   toolName: "My new",
+  //   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //   link: "www.google.com",
+  //   estimated_time: "3 Months",
   //   integrates_gender: null,
   //   is_tested_online: null,
-  //   target_scale: null,
-  //   participants: null,
-  //   methods: null,
-  //   input_types: null,
-  //   limitations: null,
-  //   strengths: null,
-  //   expected_outputs: null,
-  //   human_resources: null,
-  //   key_references: null,
-  //   thematicArea: null,
+  //   target_scale: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //   participants: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
+  //   methods: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //   input_types: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  //   limitations: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
+  //   strengths: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
+  //   expected_outputs: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
+  //   human_resources: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
+  //   key_references: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem dolor sed viverra ipsum. Erat velit scelerisque in dictum non. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo nulla.",
+  //   thematicArea: 2,
   //   importanceResearcher: { design: null, implementation: null, me: null },
   //   importanceTechnical: { design: null, implementation: null, me: null },
   //   importanceAcademia: { design: null, implementation: null, me: null },
-  //   resources: []
+  //   resources: [
+  //     {
+  //       resourceName: "Lorem ipsum",
+  //       resourceLink: "Lorem ipsum",
+  //       resourceCategory: "Training materials"
+  //     }
+  //   ]
   // };
+
+  requestTool: RequestTool = {
+    fullname: null,
+    email: null,
+    toolName: null,
+    description: null,
+    link: null,
+    estimated_time: null,
+    integrates_gender: null,
+    is_tested_online: null,
+    target_scale: null,
+    participants: null,
+    methods: null,
+    input_types: null,
+    limitations: null,
+    strengths: null,
+    expected_outputs: null,
+    human_resources: null,
+    key_references: null,
+    thematicArea: null,
+    importanceResearcher: { design: null, implementation: null, me: null },
+    importanceTechnical: { design: null, implementation: null, me: null },
+    importanceAcademia: { design: null, implementation: null, me: null },
+    resources: []
+  };
 
   newResource = {
     resourceName: null,
