412d34db441936589c66ed533437880fe29cb91d
santiagoSS99 <santiagoxq@icloud.com>
2022-09-20 16:22:03 -0500

    :wrench: Fix(Terms and conditions): Justify text and set skip function

--- supportpacks-front/projects/aiccrasp/src/app/home/results/results.component.ts
+++ supportpacks-front/projects/aiccrasp/src/app/home/results/results.component.ts
@@ -52,8 +52,11 @@ import * as html2pdf from 'html2pdf.js';
   ],
 })
 export class ResultsComponent implements OnInit {
-  tcAICCRA =
-    'To continue downloading your files, please first fill in your email and then some basic information.This information will be used by AICCRA solely for impact assessment and CGIAR and Center level reporting purposes.Filling it in will greatly help us to track the use of the portal and keep improving it. This portal provides data to a very large community of users and improving its usability and efficiency is a key aspect we work on continuously. However, you may click on <a class="skip 2" (click)="onSetEmail()">Skip</a> to download links directly.';
+  tcAICCRA = {
+    txt1: `To continue downloading your files, please first fill in your email and then some basic information. This information will be used by AICCRA solely for impact assessment and CGIAR and Center level reporting purposes. Filling it in will greatly help us to track the use of the portal and keep improving it. This portal provides data to a very large community of users and improving its usability and efficiency is a key aspect we work on continuously. However, you may click on `,
+    txt2: '<a class="skip 2" (click)="onSetEmail()">Skip</a>',
+    txt3: ' to download links directly.',
+  };
 
   faUserCircle = faUserCircle;
   faClock = faClock;
