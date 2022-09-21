a09c1c9246cd5408abb8b41c0d191e9a11914ef6
santiagoSS99 <santiagoxq@icloud.com>
2022-09-21 11:13:57 -0500

    :construction_worker: Build(dmspservices & homecomponent): app_id receives value

--- supportpacks-front/projects/dmsp/src/app/services/dmsp-services.service.ts
+++ supportpacks-front/projects/dmsp/src/app/services/dmsp-services.service.ts
@@ -4,12 +4,15 @@ import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 
 const API = environment['api'];
-const app_id = environment['app_id'];
+// const app_id = environment['app_id'];
+const app_id = environment.app_id;
 
 @Injectable({
   providedIn: 'root',
 })
 export class DmspServices {
+  app_id = app_id;
+
   constructor(private http: HttpClient) {}
   /**
    *  Get roles by app_id
@@ -50,5 +53,20 @@ export class DmspServices {
    */
   getRSC(params) {
     return this.http.post<any>(`${API}/sp/guidelines/byRSC`, params).pipe();
+  }
+
+  updateImportanceLevel(params) {
+    return this.http.patch<any>(`${API}/sp/importance-level`, params).pipe();
+  }
+
+  setDownload(params: any) {
+    // let app_id = environment.app_id;
+    params.app_id = environment.app_id;
+    console.log(params);
+    return this.http.post<any>(`${API}/sp/download`, params).pipe();
+  }
+
+  getAppId() {
+    return this.app_id;
   }
 }
