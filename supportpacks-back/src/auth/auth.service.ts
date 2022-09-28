1e71570387c14a26a7c5fde252dd477931f29362
santiagoSS99 <santiagoxq@icloud.com>
2022-09-28 17:03:00 -0500

    :construction_worker: Conf(Services): Create user, Login user and hash password

--- supportpacks-back/src/auth/auth.service.ts
+++ supportpacks-back/src/auth/auth.service.ts
@@ -1,26 +1,65 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { sp_users } from './entities/user.entity';
+import { LoginUserDto, CreateUserDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  constructor(
+    @InjectRepository(sp_users)
+    private readonly userRepository: Repository<sp_users>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      const { password, ...userData } = createUserDto;
+
+      const user = this.userRepository.create({
+        ...userData,
+        password: bcrypt.hashSync(password, 10),
+      });
+
+      await this.userRepository.save(user);
+      // delete password from user json
+      delete user.password;
+
+      return user;
+      // Return JWT
+    } catch (error) {
+      // Call function when email has already been created
+      this.handleDBErrors(error);
+    }
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  async login(loginUserDto: LoginUserDto) {
+    const { password, email } = loginUserDto;
+
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { password: true, email: true },
+    });
+    if (!user) throw new UnauthorizedException('Credentials are not valid');
+
+    if (!bcrypt.compareSync(password, user.password))
+      throw new UnauthorizedException('Credentials are not valid');
+    return user;
+
+    // return JWT
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+  // Indicate that the email has already been created
+  private handleDBErrors(error: any): never {
+    if (error.code === 'ER_DUP_ENTRY')
+      throw new BadRequestException(error.sqlMessage);
+    console.log(error);
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    throw new InternalServerErrorException('Please check server logs');
   }
 }
