f32f91ba3323410954d1810f508ebbd7c3de643b
jdmunozg <juan.david.m.g1004@gmail.com>
2023-11-21 08:12:59 -0500

    feat(Changes multi project a one project)

--- support-packages-front/projects/aiccrasp/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  app_id: 3,
-  api: 'https://mxr97876-3000.use.devtunnels.ms/api',
-};
--- support-packages-front/projects/dmsp/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  app_id: 1,
-  name: 'Data Management Support Pack',
-  api_name: 'http://localhost:4410/api',
-  api: '/api',
-};
