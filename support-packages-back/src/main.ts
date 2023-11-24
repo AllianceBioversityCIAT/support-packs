ddcc075080de78edf1070f2d659ef8471b0be244
JuanCode <jucacar28@hotmail.com>
2023-11-24 09:04:31 -0500

    :bug: Refactor CORS configuration in main.ts

--- support-packages-back/src/main.ts
+++ support-packages-back/src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+/**
+ * Boots up the application by creating a NestFactory instance,
+ * enabling CORS, and listening on port 3000.
+ */
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const cors ={
-    origin: ['https://supportpacks-test.ciat.cgiar.org/'],
-    methods: 'GET, HEAD,PUT,PATCH,POST,DELETE,OPTIONS'
-  }
-
-  app.enableCors(cors)
-
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   await app.listen(3000);
 }
