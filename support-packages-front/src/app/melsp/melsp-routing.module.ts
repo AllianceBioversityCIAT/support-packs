27f2d4cbbdf443383c574838a4955bed64943b09
xKeCo <kevcollazos@gmail.com>
2024-04-22 14:26:06 -0500

    :recycle: refactor(MELSP) A2-417: update MELSP page title

--- support-packages-front/src/app/melsp/melsp-routing.module.ts
+++ support-packages-front/src/app/melsp/melsp-routing.module.ts
@@ -4,29 +4,31 @@ import { MelspComponent } from './melsp.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component:MelspComponent,
+    path: '',
+    component: MelspComponent,
+    title: 'Monitoring, Evaluation and Learning Support Pack',
     children: [
       {
-        path:'home',
-        loadChildren: () => import('./pages/home-mel/home-mel.module').then(mod => mod.HomeMelModule)
-      },
-      { 
-        path: '',
-        redirectTo: 'home',
-        pathMatch: 'full'
-        //  runGuardsAndResolvers: 'always'
+        path: 'home',
+        loadChildren: () =>
+          import('./pages/home-mel/home-mel.module').then((mod) => mod.HomeMelModule),
       },
       {
-        path:'overview',
-        loadChildren: () => import('./pages/overview/overview.module').then(mod => mod.OverviewModule)
-      }
-    ]
-  }
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'overview',
+        loadChildren: () =>
+          import('./pages/overview/overview.module').then((mod) => mod.OverviewModule),
+      },
+    ],
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MelspRoutingModule { }
+export class MelspRoutingModule {}
