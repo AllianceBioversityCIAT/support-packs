91d103d7a61e26f725845f0807bbd500871c8e51
xKeCo <kevcollazos@gmail.com>
2024-04-22 14:18:34 -0500

    :recycle: refactor(Overview) A2-417: refactor overview component styles and component file

--- support-packages-front/src/app/melsp/pages/overview/overview.component.ts
+++ support-packages-front/src/app/melsp/pages/overview/overview.component.ts
@@ -1,59 +1,75 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicesMelService } from '../../services/services-mel.service';
 
+interface Sale {
+  category_id: number;
+  id: number;
+  name: string;
+  cate_name: string;
+  resources: Resource[];
+}
+
+interface Resource {
+  acronym: string;
+  name: string;
+  importance_level: 'Very important' | 'Important' | 'Useful' | 'Optional';
+  category_id: number;
+  id: number;
+  role_id: number;
+}
+
+interface Importance {
+  importanceLevel: number;
+  styleColor: string;
+}
+
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.scss']
+  styleUrls: ['./overview.component.scss'],
 })
-export class OverviewComponent implements OnInit{
-  sales:any[] = [];
-  rojo = "red";
-  loading = true;
-  constructor(private _servicesMelspService:ServicesMelService) { }
+export class OverviewComponent implements OnInit {
+  sales: Sale[] = [];
+  loading: boolean = true;
+
+  constructor(private _servicesMelspService: ServicesMelService) {}
 
   ngOnInit(): void {
-    this. getInformation();
+    this.getInformation();
   }
 
-  getInformation(){
-    this._servicesMelspService.getToolOverview().subscribe((data)=>{
-      console.log(data);
+  getInformation(): void {
+    this._servicesMelspService.getToolOverview().subscribe((data: { result: Sale[] }) => {
       this.sales = data.result;
       this.loading = false;
     });
   }
 
-  getImportants(acronym:string, propolsal:string, data:any, id:any){
-   let important = 0; 
-   let styleColor = "";
-    let data2 = data.filter((data:any)=>{
-      return data.acronym == acronym && data.name == propolsal ;
-    });
-    if (data2.length > 0) {
-      
-      if (data2[0].importance_level === "Very important") {
-        important = 4;
-        styleColor = 'veryImportant'
-    }
-    if (data2[0].importance_level === "Important") {
-      important =  3
-      styleColor = 'important'
-    }
-    if (data2[0].importance_level == "Useful") {
-      important =  2
-      styleColor = 'useful'
-    }
-    if (data2[0].importance_level == "Optional") {
-      important =  1
-      styleColor = 'optional'
-    }
-    }
-    
+  getImportants(data: Resource): Importance {
+    let importanceLevel = 0;
+    let styleColor = '';
 
-    return {important, styleColor};
-      
-      
-    
+    if (data) {
+      switch (data.importance_level) {
+        case 'Very important':
+          importanceLevel = 4;
+          styleColor = 'veryImportant';
+          break;
+        case 'Important':
+          importanceLevel = 3;
+          styleColor = 'important';
+          break;
+        case 'Useful':
+          importanceLevel = 2;
+          styleColor = 'useful';
+          break;
+        case 'Optional':
+          importanceLevel = 1;
+          styleColor = 'optional';
+          break;
+      }
+    }
+
+    return { importanceLevel, styleColor };
   }
 }
