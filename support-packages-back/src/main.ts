2816c88f257fb9e09216e91fd418d5f7a18c5301
xKeCo <kevcollazos@gmail.com>
2024-06-14 11:41:46 -0500

    :sparkles: feat(CORS) A2-425: enable CORS in main.ts

--- support-packages-back/src/main.ts
+++ support-packages-back/src/main.ts
@@ -12,6 +12,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = env.PORT;
 
+  app.enableCors();
+
   await app
     .listen(port)
     .then(() => {
